14ff6bafcfcb803ba628e1b64b4248000c246f7a
lpjacob <32037184+lpjacob@users.noreply.github.com>
2020-05-27 21:50:04 +0100

    Create include_in_page.js

---
+++ include_in_page.js
@@ -0,0 +1,16 @@
+function delay_dtp_init(){
+	//repeat call to add new listener to child 
+	window.setTimeout(function(){
+	  var list = document.getElementsByClassName("wpcf7-field-group-add");
+	  for (var i=0, item; item = list[i]; i++) {
+		  item.addEventListener("click",delay_dtp_init, true);
+	  }
+	},300)
+	window.setTimeout(dtp_init,100);//set time delay to allow clone to be added before calling dtp_init() in date-picker
+}
+//first call trigger on page load 
+window.setTimeout(function(){
+  var list = document.getElementsByClassName("wpcf7-field-group-add");
+  for (var i=0, item; item = list[i]; i++) {
+    item.addEventListener("click",delay_dtp_init, true); //set true to allow listerer to be added in capturing phase
+},300) //set delay time to allow page to fully load
